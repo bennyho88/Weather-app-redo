7a990f44474d9ea88292c110d8b33be2e98fbd71
Benny Ho <benny.ho2530@gmail.com>
2019-10-06 22:12:42 +0200

    added new icons and javascript

--- script.js
+++ script.js
@@ -1,85 +1,101 @@
+
+// Select elements
 const notificationElement = document.querySelector('.notification');
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 
+// App data
 
-function displayWeather() {
-    iconElement.innerHTML = `<img src="/dist/icons/${weather.iconId}.png"/>`;
+const weather = {};
 
-    tempElement.innerHTML = `${weatger.temperature.value} ° <span>C</span>`;
-
-    descElement.innerHTML = weather.description ;
-
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-
-}
-    
-function celsiusToFahrenheit(temperature) {
-    (temperature * 9/5) + 32;
+weather.temperature = {
+    unit: "celsius"
 }
 
-tempElement.addEventListener('click', function() {
+// App consts and vars
 
-    if(weather.temperature.value === undefined) return;
-    if (weather.temperature.unit === "celcius") {
-        let fahrenheit = celciusToFahrenheit(weather.temperature.value);
+const KELVIN = 273;
 
-        fahrenheit = Math.floor(fahrenheit);
+// API KEY
 
-        tempElement.innerHTML = `${fahrenheit}° <span>F</span>`;
+const key = "82005d27a116c2880c8f0fcb866998a0";
 
-        weather.temperature.unit = "fahrenheit"
-    } else {
-        tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`
-
-        weather.temperature.unit = 'celcius';
-    }
-}); 
-
-if ("geolocation" in navigator) {
+// Check if browser supports geolocation
+if('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
-    notificationElement.getElementsByClassName.display = "block";
-    notificationElement.innerHTML = "<p>Browser Doesn't Support Geolocation.</p>"
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
 }
+
+// Set user's position
 
 function setPosition(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
     getWeather(latitude, longitude);
-}
+} 
+
 
 function showError(error) {
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML =  `<p>${error.message}</p>`;
 }
 
-
-// APP CONSTANTS AND VARS
-const Kelvin = 273;
-// API KEY
-const key = "3c581e97bdd6665208a6ff2c1592317e";
 
 function getWeather(latitude, longitude) {
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
-        .then (function(reponse){
+        .then(function(response) {
             let data = response.json();
             return data;
         })
-
-        .then ( function(data) {
+        .then(function(data){
             weather.temperature.value = Math.floor(data.main.temp - KELVIN);
             weather.description = data.weather[0].description;
             weather.iconId = data.weather[0].icon;
             weather.city = data.name;
             weather.country = data.sys.country;
         })
-
-        .then (function() {
+        .then(function(){
             displayWeather();
-        })
+        });
 }
+
+// display weather to ui
+
+function displayWeather() {
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9/5) + 32;
+}
+
+
+// When the user clicks on the temperature element
+tempElement.addEventListener('click', function() {
+    if(weather.temperature.value === undefined) return;
+
+    if(weather.temperature.unit == "celsius") {
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    } else {
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius";
+    }
+
+     
+})
